3f38de452137a10fcece43208f5ef362b9c5311b
iqbalatma <iqbalatma@gmail.com>
2020-04-17 05:36:36 +0700

    Membuat activity register

---
+++ app/src/main/java/com/example/pertemuan2/Register.java
@@ -0,0 +1,23 @@
+package com.example.pertemuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+    /*
+        NIM     : 10117124
+        Nama    : Iqbal Atma Muliawan
+        Kelas   : IF4
+        Matkul  : AKB
+        Tugas   : Tugas 2
+
+        Pengerjaan
+        Tanggal 17 April
+    */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
